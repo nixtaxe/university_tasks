b44d79aa00aad6058095b39b869ceeb6203876d8
nixtaxe <fenix_dz@mail.ru>
2018-03-22 21:10:43 +1000

    Fixed circle-circle intersection for two points

--- Intersections/src/GeometricFigures.cpp
+++ Intersections/src/GeometricFigures.cpp
@@ -3,6 +3,7 @@
 //
 
 #define _USE_MATH_DEFINES
+#define EPS 2-16
 
 #include "GeometricFigures.h"
 
@@ -28,23 +29,33 @@ Circle :: intersect ( Circle& circle )
   Point c1 = this->getCenter();
   Point c2 = circle.getCenter();
 
-  double dist = sqrt((c1.x - c2.x) * (c1.x - c2.x) + 
-                       (c1.y - c2.y) * (c1.y - c2.y));
-  double maxR = max(this->getRadius(), circle.getRadius());
-  double minR = min(this->getRadius(), circle.getRadius());
+  double dist = sqrt(pow(c1.x - c2.x, 2.0) + pow(c1.y - c2.y, 2.0));
+  double r1 = this->getRadius();
+  double r2 = circle.getRadius();
+  double maxR = max(r1, r2);
+  double minR = min(r1, r2);
 
-  if (dist >= (maxR + minR) || ((maxR >= (dist + minR))))
+  bool isTooFar = dist >= (maxR + minR);
+  bool isOneInsideAnother = maxR >= (dist + minR);
+  bool isEqual = dist < EPS && (maxR - minR) < EPS;
+  if (isTooFar || isOneInsideAnother || isEqual)
     return vector<Point> {};
-  
-  double x = (dist * dist - minR * minR + maxR * maxR) /
-                (2.0 * dist);
 
-  double y = sqrt((-dist + minR - maxR) * (-dist - minR + maxR) *
-                    (-dist + minR + maxR) * (dist + minR + maxR)) /
-                      (2.0 * dist);
+  //http://algolist.manual.ru/maths/geom/intersect/circlecircle2d.php
+  double a = (pow(r1, 2.0) - pow(r2, 2.0) + pow(dist, 2.0)) / (2.0 * dist);
+  double h = sqrt(pow(r1, 2.0) - pow(a, 2.0));
 
-  return vector<Point> {{min(c1.x, c2.x) + x, c1.y + y},
-                          {min(c1.x, c2.x) + x, c1.y - y}};
+  double xh = c1.x + a * (c2.x - c1.x) / dist;
+  double yh = c1.y + a * (c2.y - c1.y) / dist;
+
+  double deltaX = h * (c2.y - c1.y) / dist;
+  double deltaY = h * (c2.x - c1.x) / dist;
+  double x1 = xh + deltaX;
+  double y1 = yh - deltaY;
+  double x2 = xh - deltaX;
+  double y2 = yh + deltaY;
+
+  return vector<Point> {{x1, y1}, {x2, y2}};
 }
 
 vector<Point>
--- Intersections/src/main.cpp
+++ Intersections/src/main.cpp
@@ -4,24 +4,27 @@
 
 #define CATCH_CONFIG_MAIN
 #define _USE_MATH_DEFINES
+#define EPS 2e-16
 
 #include <cmath>
 #include "catch.h"
 #include "GeometricFigures.h"
 
-TEST_CASE("Circle-circle intersection", "[]") {
-    SECTION("Check circle length") {
-        Circle c1((Point){.x = 0, .y = 0}, 1);
-        REQUIRE(c1.length() == 2.0 * M_PI);
-    }
-}
+TEST_CASE("Circle tests", "[]") {
+  SECTION("Check circle length") {
+    Circle c1((Point){.x = 0, .y = 0}, 1);
+    REQUIRE(c1.length() == 2.0 * M_PI);
+  }
+  SECTION("Check circle-circle intersection") {
+    WHEN("Circles have two intersection points") {
+      Circle c1((Point){.x = 0, .y = 0}, 1);
+      Circle c2((Point){.x = 1, .y = 0}, 1);
 
-//int main(int argc, char* argv[])
-//{
-//  Point p = {1.0, 1.0};
-//  Circle c((Point){.x = 1, .y = 2}, 3);
-//  cout << c.getRadius ();
-//  cout << c.getCenter ().x;
-//  cout << c.getCenter ().y;
-//  return 0;
-//}
+      THEN("Return vector with two points") {
+        vector <Point> result = c1.intersect(c2);
+        REQUIRE (abs(result[0].x - 0.5) < EPS);
+        REQUIRE (abs(result[1].x - 0.5) < EPS);
+      }
+    }
+  }
+}
